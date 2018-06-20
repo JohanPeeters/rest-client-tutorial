a7f6a513dec379d77cb018364a0e30ba8b4f4a06
johanpeeters <yo@johanpeeters.com>
2018-06-20 17:58:14 +0200

    initial commit

---
+++ src/News.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class News extends Component {
+
+  constructor(options) {
+    super(options)
+    this.state = {}
+  }
+  componentDidMount() {
+    axios({
+        baseURL: process.env.REACT_APP_API,
+        url: '/news',
+        headers: {'x-api-key': process.env.REACT_APP_API_KEY}
+    }).then(res => {
+      this.setState({message: res.data})
+    })
+  }
+  render() {
+    return (
+      <div>
+        {this.state.message &&
+          this.state.message
+        }
+      </div>
+    )
+  }
+}
+
+export default News
